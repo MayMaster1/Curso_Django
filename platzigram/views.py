e7ce5231008beabfae0ab95a3014c64e41f93b3f
Ivan Barillas <mi.barillas25@gmail.com>
2020-08-28 23:35:51 -0500

    final de 02_Solucion al reto -Tema02 Vistas C148

--- platzigram/views.py
+++ platzigram/views.py
@@ -10,12 +10,22 @@ def hello_world(request):
     return HttpResponse('la hora es . .{now}'.format(now=now))
 
 
-def hi(request):
-    """"Hi"""
+def sort_integers(request):
+    """"Return a JSON response with sorted integers"""
     
     numbers = [int(i) for i in request.GET['numbers'].split(',')]
     sorted_ints = sorted(numbers)
     #import pdb; pdb.set_trace()
     return HttpResponse(str(numbers),content_type='application/json')
+
+def say_hi(request, name, age):
+    if age < 12:
+        message = 'Sorry{}. no puedes ingresar al sitio.'.format(name)
+    else:
+        message = 'Hola {}. Bienvenida.'.format(name)
+    return HttpResponse(message)
+
+
+
 
     
